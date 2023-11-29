3aea5b388c26aad6099162eaf1d3e7c4920f268e
ankit.podder <ankitpodder0211@gmail.com>
2023-11-30 00:05:24 +0530

    handle error

--- app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.io.File
 
+
 private const val TAG = "MainViewModel"
 class MainViewModel: ViewModel() {
 
@@ -29,31 +30,57 @@ class MainViewModel: ViewModel() {
         }
     }
 
-    private fun getFileContent(file: File): ByteArray {
+    private fun getFileContent(
+        file: File
+    ): ByteArray {
         return file.readBytes()
     }
 
-    fun handleButtonClick(rootPath: String) {
+    fun handleButtonClick(
+        rootPath: String
+    ) {
+        listOfFiles.clear()
         CoroutineScope(Dispatchers.IO).launch {
             Log.d(TAG, "handleButtonClick: rootPath: $rootPath")
             val rootFile = File(rootPath)
             createFileList(rootFile)
             val deferredList = arrayListOf<Deferred<Int>>()
-            for(file in listOfFiles) {
-                val content = getFileContent(file)
-                val defObj = printContentDetails(file, content)
-                deferredList.add(defObj)
+            val size = listOfFiles.size
+            var i = 0
+            while(i < size) {
+                val file = listOfFiles[i]
+                try {
+                    val content = getFileContent(file)
+                    val defObj = printContentDetails(file, content)
+                    deferredList.add(defObj)
+                    i++
+                } catch (e: OutOfMemoryError) {
+                    // Log.e(TAG, "handleButtonClick: error : oom error getAvailableInternalMemorySize : ${getAvailableInternalMemorySize()}}")
+                    deferredList.awaitAll()
+                    deferredList.clear()
+                } catch (e: Exception) {
+                    Log.e(TAG, "handleButtonClick: exception : ${e.localizedMessage}")
+                }
             }
             deferredList.awaitAll()
-            Log.d(TAG, "handleButtonClick: Completed..................")
+            Log.d(TAG, "handleButtonClick: completed..")
         }
     }
     
     private fun printContentDetails(file: File, content: ByteArray): Deferred<Int> {
         return CoroutineScope(Dispatchers.IO).async {
-            delay(20 * 1000)
-            Log.d(TAG, "handleButtonClick: file name : ${file.name} size : ${content.size}")
+            delay(2 * 1000)
+            Log.d(TAG, "handleButtonClick: file name : ${file.name} size : ${content.size} thread name : ${Thread.currentThread().name}")
             1
         }
     }
+
+    /*private fun getAvailableInternalMemorySize(): Long{
+        val path = Environment.getDataDirectory()
+        Log.d(TAG, "getAvailableInternalMemorySize: path: ${path.path}")
+        val stat = StatFs(path.path)
+        val blockSize = stat.blockSizeLong
+        val availableBlocks = stat.availableBlocksLong
+        return availableBlocks * blockSize
+    }*/
 }
